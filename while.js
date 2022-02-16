cf8624c296d85d11fd972c69a85e9972ff7a8e32
RafDes21 <israeltineo84@gmail.com>
2022-02-16 17:02:22 -0300

    cambios

--- while.js
+++ while.js
@@ -1,15 +1,23 @@
 alert("Un jueguito por este mes de Febrero")
 let name = prompt("Hola, ingrese su nombre para jugar")
 let palabra = ""
+let contador = 0;
 
 while (palabra != "respeto") {
+    
+    palabra = prompt("¿Cuál es la palabra escondida? reddspaeesstfgfo")
+    contador ++
 
-    palabra = prompt("ingrese una palabra")
+    
     if (palabra == "respeto") {
-
-        document.write(`!!!GAAAANAAAAASSSSTEEEE ${name}, respeto era la palabra escondida`)
         
+        document.write(`!!!GAAAANAAAAASSSSTEEEE!!!--   ${name}<br>`)
+      
     }
-
+    if (contador == 1) {
+        document.write(`Lo siento ${name} perdiste...`)
+        break
+    }
+   
 }
 
